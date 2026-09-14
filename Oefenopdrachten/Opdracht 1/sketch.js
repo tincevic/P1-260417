52cc947798e9f0be45ca2db9733cf976ff0838be
tincevic <tinmartincevicfalatov@gmail.com>
2026-09-14 11:40:59 +0200

    oo

--- Oefenopdrachten/Opdracht 1/sketch.js
+++ Oefenopdrachten/Opdracht 1/sketch.js
@@ -1,7 +1,41 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(220);
+  // noStroke();
+  background(255);
+  strokeWeight(4);
+  fill(232,12,12); // rood
+  rect(2.5,2.5,55,95);
+  rect(290,2.5,98,32);
+  rect(388,100,209.5,234);
+
+  fill(12,12,232); // blauw
+  rect(57.5,2.5,97.5,54);
+  rect(344,34.5,44,54);
+  rect(2.5,282.5,55,60);
+  rect(57.5,342.5,97.5,55);
+  
+  fill(240,242,242); // wit -- mag elkaar aanraken
+  rect(155,2.5,135,86);
+  rect(57.5,56.5,97.5,286);
+  rect(388,2.5,112,80);
+  rect(369,88.5,19,245.5);
+  rect(238,183.5,131,150.5);
+  rect(155,266.5,83,131);
+  rect(238,334,212,63.5);
+  rect(513.5,334,84,63.5);
+  
+  fill(242,242,24); // geel
+  rect(2.5,97.5,55,185);
+  rect(155,88.5,214,95);
+  rect(388,82.5,112,17.5);
+
+  fill(12,12,12); // zwart -- ik maak ze allemaal vierkanten
+  square(155,183.5,83);
+  square(290.5,34.5,54);
+  square(500,2.5,97.5);
+  square(2.5,342.5,55);
+  square(450,334,63.5);
 }
--- Oefenopdrachten/Opdracht 2/sketch.js
+++ Oefenopdrachten/Opdracht 2/sketch.js
@@ -1,7 +1,61 @@
+let x = 20;
+let y = 20;
+let groet = "greetings";
+let isAWhole;
+let isAStr;
+let isBWhole;
+let isBStr;
+let a;
+let b;
+let addition;
+let subtraction;
+let multi;
+let division;
+  
 function setup() {
   createCanvas(400, 400);
+  a = round(random(100), 1); // ik rond alles hierna af op 3 decimalen want soms wordt er bij optellen bijvoorbeeld 1285.00000000000001 weergegeven
+  b = round(random(50), 1);
+  addition = round(a+b, 3); // ik tel a en b op
+  subtraction = round(a-b, 3); // ik tref b van a af
+  multi = round(a*b, 3); // ik vermenigvuldig a met b
+  division = round(a/b, 3); // ik deel a door b en rond af op 3 decimalen
+  if (a == floor(a)) {
+    isAWhole = true;
+    isAStr = "yes";
+  } else {
+    isAWhole = false;
+    isAStr = "no";
+  }
+  if (b == floor(b)) {
+    isBWhole = true;
+    isBStr = "yes";
+  } else {
+    isBWhole = false
+    isBStr = "no";
+  }
 }
 
 function draw() {
   background(220);
+  y = 20;
+  text(x,x,y);
+  y+=40;
+  text(groet,x,y);
+  y+=20; // na elke regel gaat y met 20 omhoog. x blijft hetzelfde
+  text("a = "+str(a),x,y); // ik moet str() gebruiken omdat de variabelen integers gebruiken en text() kan alleen strings weergeven
+  y+=20;
+  text("b = "+str(b),x,y);
+  y+=20;
+  text("addition: "+str(addition),x,y);
+  y+=20;
+  text("subtraction: "+str(subtraction),x,y);
+  y+=20;
+  text("multiplication: "+str(multi),x,y);
+  y+=20;
+  text("division (rounded to 3 decimals): "+str(division),x,y);
+  y+=40;
+  text("is a whole? "+isAStr,x,y);
+  y+=20;
+  text("is b whole? "+isBStr,x,y);
 }
