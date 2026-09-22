5bc26dbb71617464bc3928b9ce787044cefdc552
tincevic <tinmartincevicfalatov@gmail.com>
2026-09-22 09:49:37 +0200

    upd

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -1,5 +1,6 @@
 let img;
 let xo;
+let startxo;
 let debugActive = false;
 let sound;
 let sq1 = 0;
@@ -13,11 +14,13 @@ let sq8 = 0;
 let sq9 = 0;
 let started = false;
 let ended = false;
+let nsdet = false;
 
 function preload() {
   img = loadImage('/../Assets/hout.jpg'); // hout textuur laden
   img2 = loadImage('/../Assets/houtach.jpg'); // hout 2
-  sound = loadSound('/../Assets/ostaris.mp3');
+  sound = loadSound('/../Assets/ostaris.mp3'); // muziekje
+  chalk = loadSound('/../Assets/chalk.mp3'); // geluid
 }
 function setup() {
   createCanvas(1600,900);
@@ -32,8 +35,9 @@ function setup() {
   mG2.rect(0,0,1600,900);
   img2.mask(mG2);
   img.mask(maskGraphics);
+  startxo = 1;
   xo = 1; // 1 = x, 2 = o
-  sound.setVolume(0.3);
+  sound.setVolume(0.4);
 }
 
 function draw() {
@@ -74,74 +78,128 @@ function draw() {
       text("Start!",720,817.5);
     } else if (ended === true) {
       text("Play!",740,817.5);
+      if (startxo === 1 && nsdet === false) {
+        xo = 2;
+        startxo = 2;
+        nsdet = true;
+      } else if (startxo === 2 && nsdet === false) {
+        xo = 1;
+        startxo = 1;
+        nsdet = true;
+      }
     }
   }
   if (started === true) {
+  if (xo === 1) {
+    noStroke();
+    push();
+    translate(100,600);
+    textSize(450);
+    textStyle(BOLD);
+    angleMode(DEGREES);
+    rotate(15);
+    fill(180,180,180,220);
+    text("X",15,0)
+    fill(230,230,230,230);
+    text("X",0,0);
+    pop();
+  } else if (xo === 2) {
+    noStroke();
+    push();
+    translate(100,600);
+    textSize(450);
+    textStyle(BOLD);
+    angleMode(DEGREES);
+    rotate(15);
+    fill(50,50,50,220);
+    text("O",15,0)
+    fill(20,20,20,230);
+    text("O",0,0);
+    pop();
+  }
   stroke(255); // X en O tekenen
   strokeWeight(5);
   if (sq1 === 1) {
+    stroke(255);
     line(630,280,710,360);
     line(710,280,630,360);
   } else if (sq1 === 2) {
-    fill(0,0,0,0);
+    stroke(60);
+    fill(30,30,30,80);
     circle(670,320,90);
   }
   if (sq2 === 1) {
+    stroke(255);
     line(760,280,840,360);
     line(840,280,760,360);
   } else if (sq2 === 2) {
-    fill(0,0,0,0);
+    stroke(60);
+    fill(30,30,30,80);
     circle(800,320,90);
   }
-  if (sq3 === 1) {
+  if (sq3 === 1) { 
+    stroke(255);
     line(890,280,970,360);
     line(970,280,890,360);
   } else if (sq3 === 2) {
-    fill(0,0,0,0);
+    stroke(60);
+    fill(30,30,30,80);
     circle(930,320,90);
   }
   
   if (sq4 === 1) {
+    stroke(255);
     line(630,410,710,490);
     line(710,410,630,490);
   } else if (sq4 === 2) {
-    fill(0,0,0,0);
+    stroke(60);
+    fill(30,30,30,80);
     circle(670,450,90);
   }
   if (sq5 === 1) {
+    stroke(255);
     line(760,410,840,490);
     line(840,410,760,490);
   } else if (sq5 === 2) {
-    fill(0,0,0,0);
+    stroke(60);
+    fill(30,30,30,80);
     circle(800,450,90);
   }
   if (sq6 === 1) {
+    stroke(255);
     line(890,410,970,490);
     line(970,410,890,490);
   } else if (sq6 === 2) {
-    fill(0,0,0,0);
+    stroke(60);
+    fill(30,30,30,80);
     circle(930,450,90);
   }
 
   if (sq7 === 1) {
+    stroke(255);
     line(630,540,710,620);
     line(710,540,630,620);
   } else if (sq7 === 2) {
-    fill(0,0,0,0);
+    stroke(60);
+    fill(30,30,30,80);
     circle(670,580,90);
   }
   if (sq8 === 1) {
+    stroke(255);
     line(760,540,840,620);
     line(840,540,760,620);
   } else if (sq8 === 2) {
-    fill(0,0,0,0);
+    stroke(60);
+    fill(30,30,30,80);
     circle(800,580,90);
   }
   if (sq9 === 1) {
+    stroke(255);
     line(890,540,970,620);
     line(970,540,890,620);
   } else if (sq9 === 2) {
-    fill(0,0,0,0);
+    stroke(60);
+    fill(30,30,30,80);
     circle(930,580,90);
   }
   } else if (started != true && started != false) {
@@ -149,7 +207,9 @@ function draw() {
     text("ERROR!",800,850); // dit wordt nooit geactiveerd maar ik heb het alsnog hier staan
   }
 
+  stroke(255);
   strokeWeight(10);
+  stroke(255);
   if (((sq1 === 1) && (sq2 === 1) && (sq3 === 1)) || ((sq1 === 2) && (sq2 === 2) && (sq3 === 2))) {
     ended = true;
     line(625,320,975,320);
@@ -174,7 +234,9 @@ function draw() {
   } else if (((sq1 === 1) && (sq5 === 1) && (sq9 === 1)) || ((sq1 === 2) && (sq5 === 2) && (sq9 === 2))) {
     ended = true;
     line(630,280,970,620);
-  } 
+  } else if (sq1 != 0 && sq2 != 0 && sq3 != 0 && sq4 != 0 && sq5 != 0 && sq6 != 0 && sq7 != 0 && sq8 != 0 && sq9 != 0) {
+    ended = true;
+  }
   // debug-menu
   if (debugActive === true) { // debug-menu. op d drukken op het keyboard activeert 'm. 
     textSize(20);
@@ -185,6 +247,7 @@ function draw() {
     rect(142.5,12.5,53,2.5);
     text("beurt: "+xo,10,40);
     text("vierkanten: "+sq1+","+sq2+","+sq3+","+sq4+","+sq5+","+sq6+","+sq7+","+sq8+","+sq9,10,60)
+    text("beurt begin: "+startxo,10,80);
   }
 }
 
@@ -201,6 +264,7 @@ function keyPressed() {
 function mouseClicked() {
   if (ended === false) {
   if ((((mouseX >= 615 && mouseY >= 265) && (mouseX <= 725 && mouseY <= 375)) && sq1 === 0) && started === true) {
+    chalk.play();
     sq1 = xo;
     if (xo === 2) {
       xo = 1;
@@ -208,6 +272,7 @@ function mouseClicked() {
       xo = 2;
     }
   } else if ((((mouseX >= 745 && mouseY >= 265) && (mouseX <= 855 && mouseY <= 375)) && sq2 === 0) && started === true) {
+    chalk.play();
     sq2 = xo;
     if (xo === 2) {
       xo = 1;
@@ -215,6 +280,7 @@ function mouseClicked() {
       xo = 2;
     }
   } else if ((((mouseX >= 875 && mouseY >= 265) && (mouseX <= 985 && mouseY <= 375)) && sq3 === 0) && started === true) {
+    chalk.play();
     sq3 = xo;
     if (xo === 2) {
       xo = 1;
@@ -222,6 +288,7 @@ function mouseClicked() {
       xo = 2;
     }
   } else if ((((mouseX >= 615 && mouseY >= 395) && (mouseX <= 725 && mouseY <= 505)) && sq4 === 0) && started === true) {
+    chalk.play();
     sq4 = xo;
     if (xo === 2) {
       xo = 1;
@@ -229,6 +296,7 @@ function mouseClicked() {
       xo = 2;
     }
   } else if ((((mouseX >= 745 && mouseY >= 265) && (mouseX <= 855 && mouseY <= 505)) && sq5 === 0) && started === true) {
+    chalk.play();
     sq5 = xo;
     if (xo === 2) {
       xo = 1;
@@ -236,6 +304,7 @@ function mouseClicked() {
       xo = 2;
     }
   } else if ((((mouseX >= 875 && mouseY >= 265) && (mouseX <= 985 && mouseY <= 505)) && sq6 === 0) && started === true) {
+    chalk.play();
     sq6 = xo;
     if (xo === 2) {
       xo = 1;
@@ -243,6 +312,7 @@ function mouseClicked() {
       xo = 2;
     }
   } else if ((((mouseX >= 615 && mouseY >= 525) && (mouseX <= 725 && mouseY <= 635)) && sq7 === 0) && started === true) {
+    chalk.play();
     sq7 = xo;
     if (xo === 2) {
       xo = 1;
@@ -250,6 +320,7 @@ function mouseClicked() {
       xo = 2;
     }
   } else if ((((mouseX >= 745 && mouseY >= 525) && (mouseX <= 855 && mouseY <= 635)) && sq8 === 0) && started === true) {
+    chalk.play();
     sq8 = xo;
     if (xo === 2) {
       xo = 1;
@@ -257,6 +328,7 @@ function mouseClicked() {
       xo = 2;
     }
   } else if ((((mouseX >= 875 && mouseY >= 525) && (mouseX <= 985 && mouseY <= 635)) && sq9 === 0) && started === true) {
+    chalk.play();
     sq9 = xo;
     if (xo === 2) {
       xo = 1;
@@ -272,6 +344,7 @@ function mouseClicked() {
     started = true;
     } else {
       ended = false;
+      nsdet = false;
     }
     sq1 = 0;
     sq2 = 0;
